dd29cab87e26435564c6624f9519cce92c4e7e47
Z1vK <Z1vK@users.noreply.github.com>
2026-06-24 18:19:05 +0800

    Add hw1 hello files and result image

---
+++ hw1/hello/hello_student.c
@@ -0,0 +1,9 @@
+#include "hello_student.h"
+#include "los_debug.h"
+
+#define STUDENT_ID "YOUR_STUDENT_ID"
+
+void HelloStudentPrint(void)
+{
+    PRINTK("hello " STUDENT_ID "\n");
+}
---
+++ hw1/hello/hello_student.h
@@ -0,0 +1,6 @@
+#ifndef HELLO_STUDENT_H
+#define HELLO_STUDENT_H
+
+void HelloStudentPrint(void);
+
+#endif
